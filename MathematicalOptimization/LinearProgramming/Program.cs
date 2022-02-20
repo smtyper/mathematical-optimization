657e1428a8aa6f83e4a934d52db52cd61bee2f88
Artem <smtyper.contact@gmail.com>
2022-02-21 00:17:40 +0300

    Test solver on task

--- MathematicalOptimization/LinearProgramming/Program.cs
+++ MathematicalOptimization/LinearProgramming/Program.cs
@@ -1,1 +1,45 @@
 ﻿using System;
+using LinearProgramming;
+using LinearProgramming.Models;
+
+var configuration = new TwoDimensionalTaskSolverConfiguration()
+{
+    ConstraintFunctions = new[]
+    {
+        new LinearFunction
+        {
+            Coefficients = new[] { -1m, 1m },
+            ConstantPart = 5,
+            ConstraintType = ConstraintType.LessThanOrEqual
+        },
+        new LinearFunction
+        {
+            Coefficients = new[] { 5m, 1m },
+            ConstantPart = 5,
+            ConstraintType = ConstraintType.LessThanOrEqual
+        },
+        new LinearFunction
+        {
+            Coefficients = new[] { 0, 1m },
+            ConstantPart = -2,
+            ConstraintType = ConstraintType.GreaterThanOrEqual
+        },
+        new LinearFunction
+        {
+            Coefficients = new[] { -1m, -1m },
+            ConstantPart = 1,
+            ConstraintType = ConstraintType.LessThanOrEqual
+        }
+    },
+    MainFunction = new LinearFunction
+    {
+        Coefficients = new[] { 1m, -2m },
+        ConstantPart = null,
+        ConstraintType = ConstraintType.LessThanOrEqual
+    },
+    Type = OptimizationType.ToMax
+};
+var solver = new TwoDimensionalTaskSolver(configuration);
+var result = solver.Solve();
+
+Console.WriteLine();
