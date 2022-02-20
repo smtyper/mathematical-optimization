c1a5126394aba2d00d885054dffcce6974167214
Artem <smtyper.contact@gmail.com>
2022-02-21 00:16:18 +0300

    Solve linear task

--- MathematicalOptimization/LinearProgramming/TwoDimensionalTaskSolver.cs
+++ MathematicalOptimization/LinearProgramming/TwoDimensionalTaskSolver.cs
@@ -17,6 +17,72 @@ namespace LinearProgramming
 
             _configuration = configuration;
         }
+
+        public Point Solve()
+        {
+            var constraintPredicates = _configuration.ConstraintFunctions
+                .Select(linearFunction => linearFunction.GetPredicate())
+                .ToArray();
+
+            bool CheckPoint(Point point) => constraintPredicates
+                .All(predicate => predicate(new[] { point.First, point.Second }));
+
+            var intersectionPoints = _configuration.ConstraintFunctions
+                .SelectMany(firstFunction => _configuration.ConstraintFunctions
+                    .Select(secondFunction => GetIntersection(firstFunction, secondFunction)))
+                .Where(point => point is not null && CheckPoint(point))
+                .ToArray();
+
+            var resultPoint = _configuration.Type switch
+            {
+                OptimizationType.ToMax => intersectionPoints
+                    .OrderByDescending(point => _configuration.MainFunction.GetResult(new[] { point.First, point.Second }))
+                    .First(),
+                OptimizationType.ToMin => intersectionPoints
+                    .OrderBy(point => _configuration.MainFunction.GetResult(new[] { point.First, point.Second }))
+                    .First(),
+                _ => throw new NotImplementedException()
+            };
+
+            return resultPoint;
+        }
+
+        private static Point GetIntersection(LinearFunction first, LinearFunction second)
+        {
+            (decimal, decimal) GetFunctionCoefficients(LinearFunction function)
+            {
+                var c1 = function.Coefficients.First();
+                var c2 = function.Coefficients.Last();
+
+                if (c1 is 0)
+                    return (0, 0);
+
+                var k = -1 * c2 / c1;
+                var b = function.ConstantPart!.Value / c1;
+
+                return (k, b);
+            }
+
+            var (k1, b1) = GetFunctionCoefficients(first);
+            var (k2, b2) = GetFunctionCoefficients(second);
+
+            if (k1 == k2)
+                return null;
+
+            var x = (b2 - b1) / (k1 - k2);
+            var y = (k1 * x) + b1;
+            var point = new Point(x, y);
+
+            return point;
+        }
+
+        private static void EnsureIsValidConfiguration(TwoDimensionalTaskSolverConfiguration configuration)
+        {
+            if (configuration.ConstraintFunctions
+                .Append(configuration.MainFunction)
+                .Any(linearFunction => linearFunction.Dimension is not 2))
+                throw new Exception("The dimension of one of the functions is not equal to two.");
+        }
     }
 
     public class TwoDimensionalTaskSolverConfiguration
