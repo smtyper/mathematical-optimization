97ad72146664ca669f4df0c105a0b5eb3b425a8b
Artem <smtyper.contact@gmail.com>
2022-02-21 00:15:57 +0300

    Add TwoDimensionalTaskSolver

---
+++ MathematicalOptimization/LinearProgramming/TwoDimensionalTaskSolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LinearProgramming.Models;
+
+namespace LinearProgramming
+{
+    public class TwoDimensionalTaskSolver
+    {
+        public record Point(decimal First, decimal Second);
+
+        private readonly TwoDimensionalTaskSolverConfiguration _configuration;
+
+        public TwoDimensionalTaskSolver(TwoDimensionalTaskSolverConfiguration configuration)
+        {
+            EnsureIsValidConfiguration(configuration);
+
+            _configuration = configuration;
+        }
+    }
+
+    public class TwoDimensionalTaskSolverConfiguration
+    {
+        public IReadOnlyCollection<LinearFunction> ConstraintFunctions { get; init; }
+
+        public LinearFunction MainFunction { get; init; }
+
+        public OptimizationType Type { get; init; }
+    }
+
+    public enum OptimizationType
+    {
+        ToMin,
+        ToMax
+    }
+}
