fc42c145d1c7fedcc2ced865e47b12c53a3a6be7
Artem <smtyper.contact@gmail.com>
2022-04-11 14:03:23 +0300

    Add task example

--- TransportTask/Program.cs
+++ TransportTask/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TransportTask;
+using TransportTask.Models;
+
+var costMatrix = new[]
+{
+    new[] { 1M, 2M, 1M, 3M },
+    new[] { 0.5M, 1M, 2M, 0.5M },
+    new[] { 2M, 1M, 1M, 2M }
+};
+var reserves = new[] { 200M, 30, 100M };
+var requests = new[] { 150M, 50, 30M, 100M };
+
+var table = new TransportTaskTable(costMatrix, reserves, requests);
+var solver = new TransportTaskSolver(table, new NorthWestCornerPrimalBasisSearcher());
+
+solver.Solve();
+
+;
